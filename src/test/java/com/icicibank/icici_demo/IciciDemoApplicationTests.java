4af206ac9c1cecddef6579e725a57c54600f964e
Ramesh A <ramesharpu@gmail.com>
2024-07-29 16:40:05 +0530

    initial commit

---
+++ src/test/java/com/icicibank/icici_demo/IciciDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icicibank.icici_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IciciDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
